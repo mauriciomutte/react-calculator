f05aeca61c3a2990a95e3ce41de8110929fb7e8c
Mauricio Mutte <litiobr@gmail.com>
2019-07-20 11:26:49 -0300

    Updates

---
+++ precache-manifest.f71f25952ebcdf9607e631e32d2df105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d891e6e6aff893a6d9191228ae815d",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "16887a0439bd2a3a22a2",
+    "url": "/react-calculator/static/css/main.b5377ede.chunk.css"
+  },
+  {
+    "revision": "32bfe6000434f04292fa",
+    "url": "/react-calculator/static/js/2.b3b50664.chunk.js"
+  },
+  {
+    "revision": "16887a0439bd2a3a22a2",
+    "url": "/react-calculator/static/js/main.de52dd49.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "87d8c369956e2be7eaa7beed9636b647",
+    "url": "/react-calculator/static/media/RobotoMono-Thin.87d8c369.ttf"
+  }
+]);
